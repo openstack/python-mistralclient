b3c1e189a03adea846280d17d9b27fdde82446ce
Kirill Izotov <enykeev@stackstorm.com>
2014-04-29 18:07:01 +0700

    Add list of tasks for an execution
    Change-Id: I15993d359d858190f5692916e24f0b0e9c500767

--- horizon_dashboard/demo_dashboard/dashboards/mistral/executions/tables.py
+++ horizon_dashboard/demo_dashboard/dashboards/mistral/executions/tables.py
@@ -4,11 +4,23 @@ from horizon import tables
 
 
 class ExecutionsTable(tables.DataTable):
-    id = tables.Column("id", verbose_name=_("ID"))
+    id = tables.Column("id",
+                       verbose_name=_("ID"),
+                       link=("horizon:mistral:executions:tasks"))
     wb_name = tables.Column("workbook_name", verbose_name=_("Workbook"))
     state = tables.Column("state", verbose_name=_("State"))
 
     class Meta:
         name = "executions"
         verbose_name = _("Executions")
-        # row_actions = (ExecuteWorkflow,)
+
+
+class TaskTable(tables.DataTable):
+    id = tables.Column("id", verbose_name=_("ID"))
+    name = tables.Column("name", verbose_name=_("Name"))
+    action = tables.Column("action", verbose_name=_("Action"))
+    state = tables.Column("state", verbose_name=_("State"))
+
+    class Meta:
+        name = "tasks"
+        verbose_name = _("Tasks")
