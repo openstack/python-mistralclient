48e2780ee0148efc186c8972ca22e572fa2433c5
Nikolay Mahotkin <nmakhotkin@mirantis.com>
2015-09-17 10:47:42 +0300

    Fixing test failing on workflow-delete
     * The issue was that we tried to delete standard workflow
       which is not allowed now.
    
    Depends-On: Ia2825408ee43a6dfc80e33fa9614e11f3ff6d97b
    
    Change-Id: I4ea9d696b194d8c861a102f21e08d65225608270

--- mistralclient/tests/functional/cli/v2/cli_tests_v2.py
+++ mistralclient/tests/functional/cli/v2/cli_tests_v2.py
@@ -194,17 +194,17 @@ class WorkflowCLITests(base_v2.MistralClientTestBase):
     def test_workflow_create_delete(self):
         init_wfs = self.mistral_admin(
             'workflow-create', params=self.wf_def)
-        wf_name = [wf['Name'] for wf in init_wfs]
+        wf_names = [wf['Name'] for wf in init_wfs]
         self.assertTableStruct(init_wfs, ['Name', 'Created at', 'Updated at'])
 
         wfs = self.mistral_admin('workflow-list')
-        self.assertIn(wf_name[0], [workflow['Name'] for workflow in wfs])
+        self.assertIn(wf_names[0], [workflow['Name'] for workflow in wfs])
 
-        for wf in wfs:
-            self.mistral_admin('workflow-delete', params=wf['Name'])
+        for wf_name in wf_names:
+            self.mistral_admin('workflow-delete', params=wf_name)
 
         wfs = self.mistral_admin('workflow-list')
-        for wf in wf_name:
+        for wf in wf_names:
             self.assertNotIn(wf, [workflow['Name'] for workflow in wfs])
 
     def test_create_wf_with_tags(self):
