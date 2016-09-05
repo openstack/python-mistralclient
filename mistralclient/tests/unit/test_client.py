7c0868443bb4944b730ce308c830988d7d256c01
Nikolay Mahotkin <nmakhotkin@mirantis.com>
2016-09-05 16:54:07 +0300

    Fixing auth for keystone v2.0
     * Patch https://review.openstack.org/364267 broke
       the compatibility with Keystone API v2.0
     * This patch fixes the backward compatibility
       with Keystone v2.0
    
    Change-Id: I612595c8460f577f5dc51714843d00e090e8c7a6

--- mistralclient/tests/unit/test_client.py
+++ mistralclient/tests/unit/test_client.py
@@ -20,10 +20,33 @@ import testtools
 from mistralclient.api import client
 
 
-AUTH_HTTP_URL = 'http://localhost:5000/v3'
+AUTH_HTTP_URL_v3 = 'http://localhost:5000/v3'
+AUTH_HTTP_URL_v2_0 = 'http://localhost:5000/v2.0'
 
 
 class BaseClientTests(testtools.TestCase):
+    @mock.patch('keystoneclient.v2_0.client.Client')
+    def test_mistral_url_from_catalog_v2(self, keystone_client_mock):
+        keystone_client_instance = keystone_client_mock.return_value
+        keystone_client_instance.auth_token = str(uuid.uuid4())
+        keystone_client_instance.project_id = str(uuid.uuid4())
+        keystone_client_instance.user_id = str(uuid.uuid4())
+
+        url_for = mock.Mock(return_value='http://mistral_host:8989/v2')
+        keystone_client_instance.service_catalog.url_for = url_for
+
+        mistralclient = client.client(
+            username='mistral',
+            project_name='mistral',
+            auth_url=AUTH_HTTP_URL_v2_0,
+            service_type='workflowv2'
+        )
+
+        self.assertEqual(
+            'http://mistral_host:8989/v2',
+            mistralclient.http_client.base_url
+        )
+
     @mock.patch('keystoneclient.v3.client.Client')
     def test_mistral_url_from_catalog(self, keystone_client_mock):
         keystone_client_instance = keystone_client_mock.return_value
@@ -31,25 +54,13 @@ class BaseClientTests(testtools.TestCase):
         keystone_client_instance.project_id = str(uuid.uuid4())
         keystone_client_instance.user_id = str(uuid.uuid4())
 
-        get_endpoints = mock.Mock()
-        get_endpoints.return_value = {
-            'workflowv2': [
-                {
-                    'url': 'http://mistral_host:8989/v2',
-                    'interface': 'public',
-                    'region': None,
-                    'region_id': None,
-                    'id': '446eca511e8d45acae0924aea42a4c9f'
-                }
-            ]
-        }
-
-        keystone_client_instance.service_catalog.get_endpoints = get_endpoints
+        url_for = mock.Mock(return_value='http://mistral_host:8989/v2')
+        keystone_client_instance.service_catalog.url_for = url_for
 
         mistralclient = client.client(
             username='mistral',
             project_name='mistral',
-            auth_url=AUTH_HTTP_URL,
+            auth_url=AUTH_HTTP_URL_v3,
             service_type='workflowv2'
         )
 
@@ -60,11 +71,22 @@ class BaseClientTests(testtools.TestCase):
 
     @mock.patch('keystoneclient.v3.client.Client')
     @mock.patch('mistralclient.api.httpclient.HTTPClient')
-    def test_mistral_url_defult(self, mock, keystone_client_mock):
-        client.client(username='mistral',
-                      project_name='misteal',
-                      auth_url="http://localhost:35357/v3")
-        self.assertTrue(mock.called)
-        params = mock.call_args
-        self.assertEqual('http://localhost:8989/v2',
-                         params[0][0])
+    def test_mistral_url_defult(self, mocked, keystone_client_mock):
+        keystone_client_instance = keystone_client_mock.return_value
+        url_for = mock.Mock(side_effect=Exception)
+        keystone_client_instance.service_catalog.url_for = url_for
+
+        client.client(
+            username='mistral',
+            project_name='mistral',
+            auth_url="http://localhost:35357/v3"
+        )
+
+        self.assertTrue(mocked.called)
+
+        params = mocked.call_args
+
+        self.assertEqual(
+            'http://localhost:8989/v2',
+            params[0][0]
+        )
