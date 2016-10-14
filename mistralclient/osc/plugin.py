c936afb6b376007571a6937eaf435d44b017e9fb
KATO Tomoyuki <kato.tomoyuki@jp.fujitsu.com>
2016-10-14 14:01:57 +0900

    Add plug-in summary for osc doc
    Stevedore Sphinx extension handles this comment.
    http://docs.openstack.org/developer/python-openstackclient/plugin-commands.html
    
    Change-Id: I466e71dcbe03ca41db0eb9593911d9b4f3591d53

--- mistralclient/osc/plugin.py
+++ mistralclient/osc/plugin.py
@@ -11,6 +11,8 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 
+"""OpenStackClient plugin for Workflow service."""
+
 import logging
 
 from osc_lib import utils
