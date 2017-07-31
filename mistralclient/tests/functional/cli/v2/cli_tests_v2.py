9ec1235dff70eb8b3ba91ce4ab6ce4ba75a412e1
Andras Kovi <akovi@nokia.com>
2017-07-31 16:18:23 +0200

    Add test for target parameters and fix requests lib error
    1. Adds test for target-* parameters.
    2. Fixes an error that is caused by requests not accepting non-string
       header parameters https://github.com/requests/requests/issues/3477
    
    Closes-Bug: 1708099
    Change-Id: Ia5d1ad5062751c15928cbfd7709665a3ed769603

--- mistralclient/tests/functional/cli/v2/cli_tests_v2.py
+++ mistralclient/tests/functional/cli/v2/cli_tests_v2.py
@@ -2038,3 +2038,21 @@ class NegativeCLITests(base_v2.MistralClientTestBase):
             'action-execution-update',
             params='%s ERROR' % direct_ex_id
         )
+
+    def test_target_action_execution(self):
+        command = (
+            '--debug '
+            '--os-target-tenant-name={tenantname} '
+            '--os-target-username={username} '
+            '--os-target-password="{password}" '
+            '--os-target-auth-url="{auth_url}" '
+            '--target_insecure '
+            'run-action heat.stacks_list'
+        ).format(
+            tenantname=self.clients.tenant_name,
+            username=self.clients.username,
+            password=self.clients.password,
+            auth_url=self.clients.uri
+        )
+
+        self.mistral_alt_user(cmd=command)
