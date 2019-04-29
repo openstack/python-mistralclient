87d54bba879a90210b13bcb72c8848234007100e
Eyal <eyal.bar-ilan@nokia.com>
2019-04-29 10:19:22 +0300

    Don't use default mutable parameter
    Using default mutable parameter is bad.
    Default parameters are evaluated only once
    if you mutate it you will get unexpected results.
    
    Change-Id: I9c00cb16ac0e6cefd926974a9eab29080a4d94a7

--- mistralclient/tests/unit/base.py
+++ mistralclient/tests/unit/base.py
@@ -31,7 +31,7 @@ class BaseCommandTest(base.BaseTestCase):
         self.app = mock.Mock()
         self.client = self.app.client_manager.workflow_engine
 
-    def call(self, command, app_args=[], prog_name=''):
+    def call(self, command, app_args=(), prog_name=''):
         cmd = command(self.app, app_args)
 
         parsed_args = cmd.get_parser(prog_name).parse_args(app_args)
